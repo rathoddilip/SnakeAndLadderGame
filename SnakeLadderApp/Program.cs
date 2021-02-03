9c13396135e3fe3f143c201b1669b83e23eb41be
diliprathod32 <diliprathod32@gmail.com>
2021-02-03 17:43:30 +0530

    [Dilip] WelcomeToSnakeLadderGame

---
+++ SnakeLadderApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SnakeLadderApp
+{
+    class SnakeLadder
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Welcome to Snake and Ladder Simulation");
+            
+        }
+    }
+}
