f372bce4c81d5fcb24ec1b8c81f3f99b27023fb0
diliprathod32 <diliprathod32@gmail.com>
2021-02-03 17:58:53 +0530

    [Dilip] DiceNumber1to6

--- SnakeLadderApp/Program.cs
+++ SnakeLadderApp/Program.cs
@@ -14,7 +14,10 @@ namespace SnakeLadderApp
             int INTIAL_POSITION = 0;
             int NUMBER_OF_PLAYER = 1;
             Console.WriteLine("Welcome to Snake and Ladder Simulation");
-         
+            Random randomNumber = new Random();
+            int dieRoll = randomNumber.Next(0, 1);
+            Console.WriteLine("Number got by rolling a die: " + dieRoll);
+
 
         }
     }
